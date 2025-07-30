b5376a8d59affa6935ee55fe428bd13a5ceb1fbb
Andres Ladino <aladinoster@gmail.com>
2025-07-30 08:02:58 +0200

    feat: concurrency

---
+++ bigpic/src/sum_concurrent.rs
@@ -0,0 +1,29 @@
+use std::sync::atomic::{AtomicI32, Ordering};
+use std::sync::Arc;
+use std::thread; // Needed for AtomicI32 and Ordering
+
+fn add(n1: i32, n2: i32) -> i32 {
+    let sum = Arc::new(AtomicI32::new(n1));
+
+    let (count, increment) = if n2 > 0 { (n2, 1) } else { (-n2, -1) };
+    let mut handles = vec![];
+
+    for _ in 0..count {
+        let inner_sum = Arc::clone(&sum);
+        handles.push(thread::spawn(move || {
+            inner_sum.fetch_add(increment, Ordering::SeqCst);
+        }));
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    sum.load(Ordering::SeqCst)
+}
+
+fn main() {
+    let a = 38;
+    let b = 4;
+    println!("{} + {} = {}", a, b, add(a, b));
+}
